0f8f8e62782a7eb7e487b3a60f0a1df90c6fec41
Zack Winchell <zackwinchell@gmail.com>
2016-01-12 23:50:54 -0800

    added color differing between deposits and withdrawls

--- main.js
+++ main.js
@@ -2,12 +2,35 @@ $(document).ready(function(){
   var $balance = $('#balanceRight');
   var balance = 1000.00;
   var $addTransaction = $('#transactionSubmit');
-  var $transactionTemplate = $('tr#template').clone().attr('id', '');
+  var $transactionTemplate = $('tr#template').clone().attr('id', '').removeClass("hidden-data");
   var $description = $('#transactionDesc');
   var $date = $('#transactionDate');
   var $amount = $('#transactionAmount');
   var $mainTable = $('#mainTable')
   var $body = $('body');
+  var $filterDeposits = $('#filterDeposits');
+  var $filterWithrawls = $('#filterWithrawls');
+  var $filterReset = $('#filterReset');
+  var filtering = null;
+
+  $filterDeposits.click(function(){
+    $('.deposit').removeClass("hidden-data");
+    $('.withdrawl').addClass("hidden-data");
+    filtering = "d";
+  });
+
+  $filterWithrawls.click(function(){
+    $('.withdrawl').removeClass("hidden-data");
+    $('.deposit').addClass("hidden-data");
+    filtering = "w";
+  });
+
+  $filterReset.click(function(){
+    $('.withdrawl').removeClass("hidden-data");
+    $('.deposit').removeClass("hidden-data");
+    filtering = null;
+  })
+
 
   $addTransaction.click(function(e){
     e.preventDefault();
@@ -18,7 +41,7 @@ $(document).ready(function(){
     var date = moment($date.val());
     var description = $description.val();
 
-    if(amount === 0){
+    if(!amount){
       swal("Transaction amount is invalid","","error");
       canProceed = false;
     }
@@ -31,7 +54,11 @@ $(document).ready(function(){
         canProceed = false;
       }
       else if(moment().format("x") > date.format("x")){
-        swal("Date must not be in the past", "", "error");
+        swal("Date is invalid", "", "error");
+        canProceed = false;
+      }
+      else if(+amount === 0.00){
+        swal("Amount cannot be 0");
         canProceed = false;
       }
       if(canProceed) {
@@ -49,11 +76,13 @@ $(document).ready(function(){
       if(description.toLowerCase() === "100 cades") hundredCades();
       $transaction.find('.formTransaction').html(description);
       $transaction.find('.formDate').html(date.format("MM-DD-YYYY"));
-      if(amount > 1){
+      if(amount > 0){
         $transaction.find('.formDeposit').html("$"+amount);
+        $transaction.addClass('deposit');
       }
       else{
         $transaction.find('.formWithdrawl').html("$"+amount);
+        $transaction.addClass('withdrawl');
       }
       writeBalance(amount);
       $mainTable.append($transaction);
